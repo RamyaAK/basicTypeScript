bbbacc1a0e9e766b8d97eb7e993378505f7bc9d5
Ramya Kulkarni <ramyakulkarni@Ramyas-MacBook-Air.local>
2023-10-06 16:48:48 +0530

    basic typescript code

---
+++ app.js
@@ -0,0 +1,5 @@
+var person_name;
+var person_age;
+person_name = 'ramya';
+person_age = 20;
+console.log(person_name, person_age);
---
+++ app.ts
@@ -0,0 +1,7 @@
+let person_name: string;
+let person_age:number;
+
+person_name='ramya';
+person_age=20;
+
+console.log(person_name, person_age);
